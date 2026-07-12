760fa612a4fbbb4c00129b31d487d0d057ca9482
Samora254 <98497502+samora254@users.noreply.github.com>
2026-07-12 19:54:39 +0300

    Prepare Android 1.2.4 Play release

--- native-app/app.config.js
+++ native-app/app.config.js
@@ -46,7 +46,7 @@ module.exports = {
     name: 'Kitabu AI',
     slug: 'kitabu-ai',
     scheme: 'kitabu',
-    version: '1.2.3',
+    version: '1.2.4',
     assetBundlePatterns: ['**/*'],
     plugins: [
       [
